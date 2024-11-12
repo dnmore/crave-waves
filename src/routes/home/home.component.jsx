8eb6bd6a1963ae9a81f1612d7e3c997602df7c27
Diana Moretti <morettidn@gmail.com>
2024-11-12 15:26:22 +0100

    update home

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,9 +1,10 @@
 import hero from "../../assets/crave-hero.webp";
 
-import { HeroContainer } from "./home.styles";
+import { HeroContainer, HomeContent } from "./home.styles";
 
 const Home = () => {
   return (
+    <>
     <HeroContainer>
       <picture>
         <source type="image/webp" media="max-width:540px">
@@ -11,7 +12,18 @@ const Home = () => {
         <img loading="eager" decoding="async" src={hero} alt="hero with two sandwiches" />
       </picture>
       
+      
     </HeroContainer>
+    <HomeContent>
+    <div>
+      <h3>Explore Our Flavor Packed Menu</h3>
+      <p>
+        From juicy burgers to Tex-Mex delights, nachos, nuggets, and
+        irresistible desserts, satisfy every craving today!
+      </p>
+    </div>
+  </HomeContent>
+  </>
   );
 };
 
--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -3,7 +3,14 @@ import styled from "styled-components";
 export const HeroContainer = styled.div`
   position: relative;
   padding: 4rem 0;
-  height: 100vh;
+  height: 50vh;
+
+  @media (min-width: 1024px) {
+   
+    height: 100vh;
+  }
+  
+  
 
    img {
     object-fit: cover;
@@ -11,6 +18,36 @@ export const HeroContainer = styled.div`
     height: 100%;
     position: absolute;
     top: 0;
+  }
+`;
+
+export const HomeContent = styled.div`
+height: 50vh;
+  width: 100%;
+   background-color: #4caf50;
+  color: #fff;
+  padding: 2rem 1rem;
+  text-align: center;
+  display: grid;
+  place-items: center;
+
+  @media (min-width: 1024px) {
+   height: 100vh;
+    padding: 4rem 6rem;
+  }
+  h3 {
+    font-family: "Poppins", sans-serif;
+    font-size: 30px;
+    margin-bottom: 10px;
+    @media (min-width: 768px) {
+      font-size: 45px;
+    }
+  }
+  p {
+    font-size: 20px;
+  }
+  @media (min-width: 768px) {
+    font-size: 35px;
   }
 `;
 
