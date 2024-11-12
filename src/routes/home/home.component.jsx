9246340abaf0b568d350791b539d41e0c19c23ee
Diana Moretti <morettidn@gmail.com>
2024-11-12 14:55:56 +0100

    update home

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,25 +1,17 @@
-
-
 import hero from "../../assets/crave-hero.webp";
 
-import { HeroContainer, HomeContent } from "./home.styles";
+import { HeroContainer } from "./home.styles";
 
 const Home = () => {
   return (
-    <div>
-      <HeroContainer
-        style={{ backgroundImage: `url(${hero})` }}
-      ></HeroContainer>
-      <HomeContent>
-        <div>
-          <h3>Explore Our Flavor Packed Menu</h3>
-          <p>
-            From juicy burgers to Tex-Mex delights, nachos, nuggets, and
-            irresistible desserts, satisfy every craving today!
-          </p>
-        </div>
-      </HomeContent>
-    </div>
+    <HeroContainer>
+      <picture>
+        <source type="image/webp" media="max-width:540px">
+        </source>
+        <img loading="eager" decoding="async" src={hero} alt="hero with two sandwiches" />
+      </picture>
+      
+    </HeroContainer>
   );
 };
 
--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -1,46 +1,16 @@
 import styled from "styled-components";
 
-
 export const HeroContainer = styled.div`
-  background-size: cover;
-  background-position: center;
-  height: 50vh;
-  width: 100%;
+  position: relative;
+  padding: 4rem 0;
+  height: 100vh;
 
-  @media (min-width: 768px) {
-    height: 100vh;
+   img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
   }
 `;
 
-export const HomeContent = styled.div`
-  height: 50vh;
-  width: 100%;
-   background-color: #4caf50;
-  color: #fff;
-  padding: 2rem 1rem;
-  text-align: center;
-  display: grid;
-  place-items: center;
-
-  @media (min-width: 768px) {
-    height: 100vh;
-    padding: 4rem 6rem;
-  }
-
-  h3 {
-    font-family: "Poppins", sans-serif;
-    font-size: 30px;
-    margin-bottom: 10px;
-
-    @media (min-width: 768px) {
-      font-size: 45px;
-    }
-  }
-
-  p {
-    font-size: 20px;
-  }
-  @media (min-width: 768px) {
-    font-size: 35px;
-  }
-`;
