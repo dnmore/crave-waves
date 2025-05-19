24e81951e6914db906fa5f04217a86348801e971
Diana Moretti <morettidn@gmail.com>
2025-05-19 16:41:51 +0200

    update menu styles

--- src/routes/menu/menu.component.tsx
+++ src/routes/menu/menu.component.tsx
@@ -1,28 +1,26 @@
 import { useEffect, Fragment } from "react";
 
-
 import ProductCard from "../../components/product-card/product-card.component";
 
-import {MenuContainer, MenuSection, MenuTitle } from "./menu.styles";
+import { MenuContainer, MenuSection, MenuTitle } from "./menu.styles";
 import { fetchCategoriesAsync } from "../../store/categories/categoriesSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
 const Menu = () => {
-  
-  const dispatch = useAppDispatch()
-  const categoriesMap = useAppSelector((state) => state.categories.categoriesMap)
+  const dispatch = useAppDispatch();
+  const categoriesMap = useAppSelector(
+    (state) => state.categories.categoriesMap
+  );
 
   useEffect(() => {
-    dispatch(fetchCategoriesAsync())
-  }, [dispatch])
+    dispatch(fetchCategoriesAsync());
+  }, [dispatch]);
 
   return (
     <MenuContainer>
       {Object.keys(categoriesMap).map((title) => (
         <Fragment key={title}>
-          <MenuTitle>
-            {title}
-          </MenuTitle>
+          <MenuTitle>{title}</MenuTitle>
 
           <MenuSection>
             {categoriesMap[title].map((product) => (
